6a8374179e3cea614ff9b68166af2644671d525f
thebradsters <thebradsters@gmail.com>
2022-02-04 21:19:09 -0500

    Completed HW 3

--- assignments/hw3/hw3.py
+++ assignments/hw3/hw3.py
@@ -12,24 +12,43 @@ I certify that this assignment is my own work, but I discussed it with: <Name(s)
 
 
 def average():
-    pass
-
+    var = eval(input("how many grades will you enter? "))
+    sum = 0
+    for i in range(1, var + 1):
+        grades = eval(input("enter grade: "))
+        sum = sum + grades
+        norm = sum / var
+    print("average is", norm)
 
 def tip_jar():
-    pass
-
+    sum = 0
+    for i in range(1, 5 + 1):
+        var = eval(input("how much would you like to donate?"))
+        sum = sum + var
+    print("total tips:", sum)
 
 def newton():
-    pass
-
+    var = eval(input("What number do you want to square root? "))
+    improve = eval(input("How many times should we improve the approximation? "))
+    sum = var
+    for i in range(improve):
+        i = i + 1
+        sum = (sum + (var / sum )) / 2
+    print("the square root is approximately", sum)
 
 def sequence():
-    pass
-
+    terms = eval(input("how many terms would you like? "))
+    for i in range(1, terms + 1):
+        print((i - 1) + (i % 2), end=" ")
 
 def pi():
-    pass
-
+    terms = eval(input("how many terms in the series? "))
+    sum = 2
+    for i in range(1, terms + 1):
+        left = (2 * i) / (2 * i - 1)
+        right = (2 * i) / (2 * i + 1)
+        sum = sum * left * right
+    print(sum)
 
 if __name__ == '__main__':
     pass
