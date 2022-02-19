77bf16a94cd9a8e91d19846b0c382763b5b303d8
thebradsters <thebradsters@gmail.com>
2022-02-18 19:19:58 -0500

    Completed HW 5

--- assignments/hw5/hw5.py
+++ assignments/hw5/hw5.py
@@ -12,31 +12,64 @@ I certify that this assignment is my own work, but I discussed it with: <Name(s)
 
 
 def name_reverse():
-    pass
+    myString = input("enter a name (first last): ")
+    firstStr, lastStr = myString.split()
+    comma = ","
+    print(lastStr + str(comma), firstStr)
 
 
 def company_name():
-    pass
+    domain = input("enter a domain: ")
+    www, website, com = domain.split(".")
+    print(website)
 
 
 def initials():
-    pass
+    students = eval(input("how many students are in the class? "))
+    for i in range(students):
+        name = input(("what is the name of student? ", i + 1))
+        firstStr, lastStr = name.split()
+        print(firstStr[0] + str(lastStr[0]))
 
 
 def names():
-    pass
+    names = input("enter a list of names: ")
+    list = names.split()
+    for initial in list:
+        initial = initial[0]
+        print(initial, end="")
 
 
 def thirds():
-    pass
+    num_sentence = eval(input("enter the number of sentences: "))
+    frag = ""
+    for i in range(1, num_sentence + 1):
+        sentences = input("enter sentence " + str(i) + ": ")
+        length = len(sentences)
+        frag = frag + sentences[0:length:3] + "\n"
+    print(frag)
 
 
 def word_average():
-    pass
+    sentence = input("enter a sentence: ")
+    list = sentence.split()
+    length = len(list)
+    letters = "".join(list)
+    word_length = len(letters)
+    average = word_length / length
+    print(average)
 
 
 def pig_latin():
-    pass
+    sentence_input = input("enter a sentence to convert to pig latin: ")
+    word = sentence_input.split()
+    pig = "ay"
+    for words in word:
+        latin = words[1:] + words[0]
+        p_l = latin + pig
+        pig_latin = p_l.lower()
+        final = pig_latin.rstrip()
+        print(final, end=" ")
 
 
 if __name__ == '__main__':
