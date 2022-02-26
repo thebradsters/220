66159dea7e3f4aead9c44e0ae6eda22a206822d8
thebradsters <thebradsters@gmail.com>
2022-02-25 21:37:39 -0500

    Completed HW 6

--- assignments/hw6/hw6.py
+++ assignments/hw6/hw6.py
@@ -9,34 +9,67 @@ Certification of Authenticity:
 I certify that this assignment is entirely my own work.
 I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
-
+import math
 
 def cash_converter():
-    pass
+    integer = float(input("enter an integer: "))
+    print("That is ${}0".format(integer))
 
 
 def encode():
-    pass
+    message = input("enter a message: ")
+    key = int(input("enter a key: "))
+    length = len(message)
+    my_string = ""
+    for i in range(length):
+        letter = message[i]
+        encoded = ord(letter) + key
+        decoded = chr(encoded)
+        my_string = my_string + decoded
+    print(my_string)
 
 
 def sphere_area(radius):
-    pass
+    area = 4 * math.pi * (radius ** 2)
+    return area
 
 
 def sphere_volume(radius):
-    pass
+    volume = (4/3) * math.pi * (radius ** 3)
+    return volume
 
 
 def sum_n(number):
-    pass
+    num_sum = 0
+    for num in range(1, number + 1):
+        num_sum = num_sum + num
+    return num_sum
 
 
 def sum_n_cubes(number):
-    pass
+    num_sum = 0
+    for num in range(1, number + 1):
+        num_sum = num_sum + (num ** 3)
+    return num_sum
 
 
 def encode_better():
-    pass
+    message = input("enter a message: ")
+    key = input("enter a key: ")
+    length = len(message)
+    key_length = len(key)
+    my_string = ""
+    for i in range(length):
+        letter = message[i]
+        number_key = i % key_length
+        letter_key = key[number_key]
+        encoded = ord(letter) - 65
+        key_encoded = ord(letter_key) - 65
+        both_encoded = encoded + key_encoded
+        encode_mod = (both_encoded % 58) + 65
+        decoded = chr(encode_mod)
+        my_string = my_string + decoded
+    print(my_string)
 
 
 if __name__ == '__main__':
